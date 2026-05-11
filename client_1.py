077e8e4c6b4f368d4ea2aea5b72907b71ac220a4
Jacek Kulik <macienias@gmail.com>
2026-05-11 19:35:10 +0200

    Naming changes for assignment 2

--- client_1.py
+++ client_1.py
@@ -10,7 +10,7 @@ from ipv8.messaging.payload_dataclass import DataClassPayload
 from ipv8.lazy_community import lazy_wrapper
 from ipv8_service import IPv8
 
-from challenger import do_challenge, EMAIL, GITHUB_URL
+from nonce_finder import do_challenge, EMAIL, GITHUB_URL
 
 COMMUNITY_ID = "2c1cc6e35ff484f99ebdfb6108477783c0102881"
 SERVER_PUBLIC_KEY = "4c69624e61434c504b3a86b23934a28d669c390e2d1fc0b0870706c4591cc0cb178bc5a811da6d87d27ef319b2638ef60cc8d119724f4c53a1ebfad919c3ac4136c501ce5c09364e0ebb"
