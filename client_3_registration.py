6e2fa6c06fcf4aa64f1c020a29462d7987d41c5f
Jacek Kulik <macienias@gmail.com>
2026-06-08 19:09:46 +0200

    Revert to old key

--- client_3_registration.py
+++ client_3_registration.py
@@ -25,7 +25,8 @@ OUR_COMMUNITY_ID = bytes.fromhex(
 SERVER_PUBLIC_KEY = bytes.fromhex(
     "4c69624e61434c504b3ae3fc099fb56ca3b5e1de9a1c843387f2acdbb78b1bd4350ffde518068a0d246344b10d0d8c355fd0d76873e7d7f7838f3715e025af08f791324495e083331ce6"
 )
-GROUP_ID = "a6edc7f90a618bd8"
+# GROUP_ID = "a6edc7f90a618bd8"
+GROUP_ID = "f9e7dc3ac7b0a791"
 KEYS_FILE = "myKeys.pem"
 
 
